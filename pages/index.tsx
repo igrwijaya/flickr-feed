6d91eb6b53292ce5344cb0ad4b02a450bb8d554f
igrwijaya <igrwijaya@gmail.com>
2021-08-19 22:52:36 +0800

    add react-infinite-scroll-component package and implement infinite scroll component to load the images (paging)

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,41 @@ import type { NextPage } from 'next'
 import Image from 'next/image'
 import DefaultLayout from "../layouts/DefaultLayout";
 import {Col, Container, Row} from "react-bootstrap";
+import {useEffect, useState} from "react";
+import InfiniteScroll from "react-infinite-scroll-component";
+
+interface Image {
+  url: string
+  alt: string
+}
 
 const Home: NextPage = () => {
+
+  const [images, setImages] = useState<Image[]>([])
+
+  useEffect(() => {
+    const images: Image[] = []
+
+    images.push({
+      url: '/assets/images/logo-aia-insurance_169.png',
+      alt: 'AIA Finance'
+    })
+    images.push({
+      url: '/assets/images/logo-aia-insurance_169.png',
+      alt: 'AIA Finance'
+    })
+    images.push({
+      url: '/assets/images/logo-aia-insurance_169.png',
+      alt: 'AIA Finance'
+    })
+    setImages(images)
+
+  }, [])
+
+  const fetchMoreData = () => {
+
+  }
+
   return (
     <DefaultLayout>
       <Container className="mt-3">
@@ -22,32 +55,20 @@ const Home: NextPage = () => {
           </Col>
         </Row>
         <hr/>
-        <Row>
-          <Col sm={6} md={4} className="text-center mb-2 p-1">
-            <Image src="/assets/images/logo-aia-insurance_169.png" width={150} height={150} alt="AIA Finance" />
-          </Col>
-          <Col sm={6} md={4} className="text-center mb-2 p-1">
-            <Image src="/assets/images/logo-aia-insurance_169.png" width={150} height={150} alt="AIA Finance" />
-          </Col>
-          <Col sm={6} md={4} className="text-center mb-2 p-1">
-            <Image src="/assets/images/logo-aia-insurance_169.png" width={150} height={150} alt="AIA Finance" />
-          </Col>
-          <Col sm={6} md={4} className="text-center mb-2 p-1">
-            <Image src="/assets/images/logo-aia-insurance_169.png" width={150} height={150} alt="AIA Finance" />
-          </Col>
-          <Col sm={6} md={4} className="text-center mb-2 p-1">
-            <Image src="/assets/images/logo-aia-insurance_169.png" width={150} height={150} alt="AIA Finance" />
-          </Col>
-          <Col sm={6} md={4} className="text-center mb-2 p-1">
-            <Image src="/assets/images/logo-aia-insurance_169.png" width={150} height={150} alt="AIA Finance" />
-          </Col>
-          <Col sm={6} md={4} className="text-center mb-2 p-1">
-            <Image src="/assets/images/logo-aia-insurance_169.png" width={150} height={150} alt="AIA Finance" />
-          </Col>
-          <Col sm={6} md={4} className="text-center mb-2 p-1">
-            <Image src="/assets/images/logo-aia-insurance_169.png" width={150} height={150} alt="AIA Finance" />
-          </Col>
-        </Row>
+        <InfiniteScroll
+            dataLength={images.length}
+            next={fetchMoreData}
+            hasMore={images.length > 12}
+            loader={<h4>Loading...</h4>}
+        >
+          <Row className="mr-0 ml-0">
+            {images.map((image, index) => (
+                <Col key={index} sm={6} md={4} className="text-center mb-2 p-1">
+                  <Image src={image.url} width={150} height={150} alt={image.alt} />
+                </Col>
+            ))}
+          </Row>
+        </InfiniteScroll>
       </Container>
     </DefaultLayout>
   )
